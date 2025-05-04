b6797c8ab377c5774120b3062ea0a0b628ff277a
dpqlsn <a01037111209@gmail.com>
2025-05-04 18:37:05 +0900

    fix(#07) :: 렌딩화면 사진 위치

--- chamna/src/pages/Landing.tsx
+++ chamna/src/pages/Landing.tsx
@@ -7,16 +7,14 @@ function Landing() {
     const navigate = useNavigate();
 
     const handleClick = () => {
-    navigate('/loading');
+        navigate('/loading');
     };
 
     return (
         <div onClick={handleClick} style={{ cursor: 'pointer' }}>
-            <div className='wrapper'>
-                <div className='logo'>
-                    <img src={logo} width='109px' />
-                    <div className='slogan'>참된 나를 추억하다</div>
-                </div>
+            <div className='wrapper' style={{ flexDirection: 'column' }}>
+                <img src={logo} width='109px' style={{ marginBottom: '20px' }} />
+                <div className='slogan'>참된 나를 추억하다</div>
             </div>
         </div>
     );
