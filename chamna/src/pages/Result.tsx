ee4dae4b7d80216d0c38c88ae5255ffe1f20720e
dpqlsn <a01037111209@gmail.com>
2025-05-06 22:39:48 +0900

    feat(#24) :: 배경 추가

--- chamna/src/pages/Result.tsx
+++ chamna/src/pages/Result.tsx
@@ -1,16 +1,28 @@
 import { useLocation } from 'react-router-dom';
+import '../App.css';
+import '../pic/Bframe.png';
+import '../pic/Sframe.png';
+import '../pic/chamna.jpg';
+import logo from '../pic/chamna.jpg';
 
 const Result: React.FC = () => {
     const location = useLocation();
     const imageSrc = location.state?.image;
 
     return (
-        <div>
-            {imageSrc ? (
-                <img src={imageSrc} alt="captured" style={{ transform: 'scaleX(-1)' }} />
-            ) : (
-                <p>이미지가 없습니다.</p>
-            )}
+        <div className="result-wrapper">
+            <img src={logo} width='67px' />
+            <div className="content-area">
+                <div className="polaroid-frame">
+                        <img
+                            src={imageSrc}
+                            alt="captured"
+                            className="photo"
+                        />
+                </div>
+            </div>
+            <div className="footer">
+        </div>
         </div>
     );
 };
