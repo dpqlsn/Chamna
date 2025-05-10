a2416bfdc03b025abe88465ce1e2fec72a4ef4ff
dpqlsn <a01037111209@gmail.com>
2025-05-10 18:22:37 +0900

    feat(#26) :: 참나 프레임 선택 화면구현

--- chamna/src/pages/Result.tsx
+++ chamna/src/pages/Result.tsx
@@ -1,6 +1,8 @@
 import { useLocation } from 'react-router-dom';
 import '../App.css';
 import logo from '../pic/chamna.jpg';
+import Bframe from '../pic/Bframe.png';
+import Sframe from '../pic/Sframe.png';
 
 const Result: React.FC = () => {
     const location = useLocation();
@@ -13,6 +15,11 @@ const Result: React.FC = () => {
                     <div className="container">
                         <div className="white-background" />
                         <img src={imageSrc} alt="captured" className="photo" />
+                    </div>
+                    <div className = "frame-area">
+                        <div className = "text">프레임 크기 선택</div>
+                        <img src={Bframe} width="67px" alt="B" style={{ marginRight: "16px" }} />
+                        <img src = {Sframe} width = "51px" alt="S" />
                     </div>
                 <div className="footer" />
             </div>
