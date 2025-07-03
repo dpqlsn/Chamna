2df36ebbd6a4f54030a2809d204e19df6556495a
dpqlsn <a01037111209@gmail.com>
2025-07-03 14:53:01 +0900

    feat (#4) :: 온보딩 화면

--- chamna/src/pages/main/main.tsx
+++ chamna/src/pages/main/main.tsx
@@ -1,8 +1,14 @@
 import * as _ from './style';
-import '../../App.css'
+import '../../App.css';
+import Chamna from '../../assets/Chamna.svg';
 
 export default function Main(){
     return(
-        <_.Container></_.Container>
+        <_.Container>
+            <_.Wrapper>
+                <img src={Chamna} alt='logo' />
+                <_.Title>참된 나를 추억하다</_.Title>
+            </_.Wrapper>
+        </_.Container>
     )
 }
--- chamna/src/pages/main/style.ts
+++ chamna/src/pages/main/style.ts
@@ -2,4 +2,19 @@ import styled from '@emotion/styled'
 
 export const Container = styled.div`
     width: 100%;
+    height: 100vh;
+    background-color: #6AACCE;
+    overflow: hidden;
+`;
+
+export const Wrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+`;
+
+export const Title = styled.div`
+    font-size: 2rem;
+    color: #F7ECDA;
 `;
