3a07abdf05a5221169db368c4d72a12886d9ba74
dpqlsn <a01037111209@gmail.com>
2025-07-07 11:53:58 +0900

    feat (#14) :: 저장된 이미지 위에 로고 추가

--- chamna/src/pages/Image/Image.tsx
+++ chamna/src/pages/Image/Image.tsx
@@ -1,16 +1,22 @@
-import { useLocation } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
+import chamna from '@_assets/Chamna.svg';
+import * as _ from './style';
 
 export default function Image() {
+    const navigate = useNavigate();
     const location = useLocation();
     const capturedImage = location.state?.capturedImage;
 
     return (
-        <>
-            {capturedImage ? (
-                <img src={capturedImage} alt="캡쳐 이미지" style={{transform: 'scaleX(-1)'}} />
-            ) : (
-                <p>이미지가 없습니다.</p>
-            )}
-        </>
+        <_.Container>
+            <_.Top>
+                <_.Logo src={chamna} alt="logo" onClick={() => navigate('/')} />
+            </_.Top>
+                {capturedImage ? (
+                    <img src={capturedImage} alt="캡쳐 이미지" style={{transform: 'scaleX(-1)'}} />
+                ) : (
+                    <p>이미지가 없습니다.</p>
+                )}
+        </_.Container>
     );
 }
---
+++ chamna/src/pages/Image/style.ts
@@ -0,0 +1,14 @@
+import styled from '@emotion/styled';
+
+export const Container = styled.div`
+    width: 100%;
+`;
+
+export const Top = styled.div`
+    width: 100%;
+`;
+
+export const Logo = styled.img`
+    width: 4%;
+    cursor: pointer;
+`;
