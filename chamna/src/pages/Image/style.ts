b8bd3be37caae558434276fbd69e977e3df48db6
dpqlsn <a01037111209@gmail.com>
2025-07-14 11:17:29 +0900

    fix :: 사진 중앙에 띄우기

--- chamna/src/pages/Image/style.ts
+++ chamna/src/pages/Image/style.ts
@@ -1,7 +1,12 @@
 import styled from '@emotion/styled';
 
 export const Container = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
     width: 100%;
+
 `;
 
 export const Top = styled.div`
