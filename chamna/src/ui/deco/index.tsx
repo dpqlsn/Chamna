0e12fd42730b486eb571daf6577d0464d777dd4e
dpqlsn <a01037111209@gmail.com>
2025-10-13 15:08:37 +0900

    docs :: 임시 이미지 불러오기

--- chamna/src/ui/deco/index.tsx
+++ chamna/src/ui/deco/index.tsx
@@ -1,9 +1,21 @@
 import * as _ from "./style";
+import { useEffect, useState } from "react";
 
 export default function Deco() {
+  const [capture, setCapture] = useState<string | null>(null);
+
+  useEffect(() => {
+    const stored = sessionStorage.getItem('capture');
+    if (stored) setCapture(stored);
+  }, []);
+
   return (
     <_.Container>
-      deco
+      {capture ? (
+        <img src={capture} alt="captured" style={{ width: '100%', height: '100vh', objectFit: 'contain' }} />
+      ) : (
+        <div>촬영된 사진이 없습니다</div>
+      )}
     </_.Container>
   );
 }
