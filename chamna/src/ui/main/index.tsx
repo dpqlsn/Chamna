0de4204312099fa3d1b567473f0b4cde82587e9f
dpqlsn <a01037111209@gmail.com>
2025-10-13 10:17:41 +0900

    feat :: 메인 퍼블리싱

--- chamna/src/ui/main/index.tsx
+++ chamna/src/ui/main/index.tsx
@@ -1,9 +1,23 @@
 import * as _ from './style';
+import Image from 'next/image';
 
 export default function Main() {
     return (
         <_.Container>
-
+            <_.Wrapper>
+                <_.Flower>
+                    <Image src="/assets/flower.svg" alt="flower" width={390} height={390} style={{ backgroundColor: 'transparent' }} />
+                </_.Flower>
+                <_.Box />
+                <_.Group>
+                    <Image src="/assets/timer.svg" alt="timer" width={50} height={50} style={{ cursor: 'pointer' }} />
+                    <Image src="/assets/camera.svg" alt="camera" width={50} height={50} style={{ cursor: 'pointer' }} />
+                </_.Group>
+            </_.Wrapper>
+            <_.Wrapper>
+                <Image src="/assets/heartflower.svg" alt="flower" width={344} height={338} />
+                <_.Frame />
+            </_.Wrapper>
         </_.Container>
     );
 }
--- chamna/src/ui/main/style.ts
+++ chamna/src/ui/main/style.ts
@@ -1,13 +1,46 @@
 import styled from "@emotion/styled";
 
-
 export const Container = styled.div`
   display: flex;
-  flex-direction: column;
   justify-content: center;
-  align-items: center;
+  gap: 2rem;
   width: 100%;
-  height: 100dvh;
-  max-height: 100dvh;
+  max-height: 100vh;
+  padding: 4rem 5rem;
+`;
+
+export const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  height: 80vh;
+  row-gap: 2rem;
+`;
+
+export const Box = styled.div`
+  display: flex;
+  width: 100%;
+  height: 100vh;
+  background-color: aqua;
+`;
+
+export const Group = styled.div`
+  display: flex;
   gap: 1rem;
+`;
+
+export const Frame = styled.div`
+  display: flex;
+  width: 45%;
+  height: 38vh;
+  background-color: beige;
+`;
+
+export const Flower = styled.div`
+  display: flex;
+  position: absolute;
+  background-color: transparent;
+  top: 0;
+  left: 0;
+  z-index: 1;
 `;
